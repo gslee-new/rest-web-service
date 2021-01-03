85b28ef82c04f05233fbca12aca0955ab4997882
gslee <gyu84@naver.com>
2021-01-03 19:12:06 +0900

    First commit

---
+++ src/main/java/com/example/restfulwebservice/controller/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.example.restfulwebservice.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+}
